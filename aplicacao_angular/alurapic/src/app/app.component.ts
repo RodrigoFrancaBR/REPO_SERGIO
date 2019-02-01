f0173558b8fb70e8ff69c012d8cf9e6da0c980aa
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2019-02-01 00:35:42 -0200

    Projeto Alura Pic
    Criação do projeto alurapic

---
+++ aplicacao_angular/alurapic/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // tslint:disable-next-line:max-line-length
+  url = 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8';
+  descricao = 'Invicto';
+  title = 'Rodrigo';
+}
