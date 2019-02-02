acdf85718bae7dee6eda05b77446bf4a6bd473d8
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2019-02-01 23:17:04 -0200

    Enviando parâmetros do template de um componente para o componete photo.

--- aplicacao_angular/alurapic/src/app/photo/photo.component.ts
+++ aplicacao_angular/alurapic/src/app/photo/photo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo.component.css']
 })
 export class PhotoComponent implements OnInit {
-// tslint:disable-next-line:max-line-length
-src = 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8';
-alt = 'Invicto';
-
+  // tslint:disable-next-line:max-line-length
+  // src = 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8';
+  // alt = 'Invicto';
+  @Input() src = '';
+  @Input() alt = '';
   constructor() { }
 
   ngOnInit() {
