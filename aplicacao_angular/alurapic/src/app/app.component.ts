d136b879f9f7e33463965bd270c900f1cfe9de54
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2019-02-01 22:38:31 -0200

    criacao do componente photo

--- aplicacao_angular/alurapic/src/app/app.component.ts
+++ aplicacao_angular/alurapic/src/app/app.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // tslint:disable-next-line:max-line-length
-  url = 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8';
-  descricao = 'Invicto';
-  title = 'Rodrigo';
+// tslint:disable-next-line:max-line-length
+// url = 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8';
+// alt = 'Invicto';
 }
--- aplicacao_angular/alurapic/src/app/app.module.ts
+++ aplicacao_angular/alurapic/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
