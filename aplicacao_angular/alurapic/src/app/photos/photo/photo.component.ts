00a82af401bb2c7f6fd1fb4ebdebdff0af112bac
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2019-02-03 21:42:49 -0200

    One Way data bind os dados trafegam do componente para o template, ou para o componente que está sendo usado no template neste caso app-photo.

--- aplicacao_angular/alurapic/src/app/photos/photo/photo.component.ts
+++ aplicacao_angular/alurapic/src/app/photos/photo/photo.component.ts
@@ -9,20 +9,23 @@ export class PhotoComponent implements OnInit {
   // tslint:disable-next-line:max-line-length
   // src = 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8';
   // alt = 'Invicto';
-  photos = [
-    {
-      // tslint:disable-next-line:max-line-length
-      src : 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32805056_1045883828901028_1834317013850259456_n.jpg?_nc_cat=102&_nc_ht=scontent.fgig4-1.fna&oh=15c53739e77ae3a8939ab4611ef049e3&oe=5CFF9ECA',
-      alt : 'Invicto1'
-    },
-    {
-      // tslint:disable-next-line:max-line-length
-      src : 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8',
-      alt : 'Invicto2'
-    }
-  ];
-  // @Input() src = '';
-  // @Input() alt = '';
+  // photos = [
+  //   {
+  //     // tslint:disable-next-line:max-line-length
+  // tslint:disable-next-line:max-line-length
+  //     src : 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32805056_1045883828901028_1834317013850259456_n.jpg?_nc_cat=102&_nc_ht=scontent.fgig4-1.fna&oh=15c53739e77ae3a8939ab4611ef049e3&oe=5CFF9ECA',
+  //     alt : 'Invicto1'
+  //   },
+  //   {
+  //     // tslint:disable-next-line:max-line-length
+        // tslint:disable-next-line:max-line-length
+  //     src : 'https://scontent.fgig4-1.fna.fbcdn.net/v/t1.0-9/32207890_1041794692643275_1138426376529903616_o.jpg?_nc_cat=109&_nc_ht=scontent.fgig4-1.fna&oh=5d9b36979e165fd72c55f50a40fcd7ea&oe=5CF983C8',
+  //     alt : 'Invicto2'
+  //   }
+  // ];
+
+   @Input() src = '';
+   @Input() alt = '';
   constructor() { }
 
   ngOnInit() {
