ef2c67967ed46c1b2749e5967d5d546d34b01f85
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2019-02-12 05:02:21 -0200

    migrando rest
    rest

--- aplicacao_rest/invictoVestibulares/src/br/com/franca/dao/ConnectionFactory.java
+++ aplicacao_rest/invictoVestibulares/src/br/com/franca/dao/ConnectionFactory.java
@@ -10,6 +10,29 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	protected static ComboPooledDataSource dataSource;
+	
+	public ConnectionFactory() {
+		if (dataSource == null) {
+			dataSource = new ComboPooledDataSource();
+			try {
+				System.out.println("Tentando iniciar o dataSource:");
+				dataSource.setDriverClass("com.mysql.jdbc.Driver");
+				dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/invicto_db?autoReconnect=true&useSSL=false");
+				dataSource.setUser("root");
+				dataSource.setPassword("root");
+				dataSource.setMinPoolSize(5);
+				dataSource.setMaxPoolSize(5);
+				System.out.println("dataSource obtido com sucesso!");
+			} catch (PropertyVetoException e) {
+				System.out.println("Ocorreu um erro na construção do dataSource" + e);
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+
+		} else {
+			System.out.println("Você já está usando um dataSource");
+		}
+	}
 
 	//Local
 	
@@ -40,7 +63,7 @@ public class ConnectionFactory {
 		
 		// Azure
 	
-	public ConnectionFactory() {
+	/*public ConnectionFactory() {
 		if (dataSource == null) {
 			dataSource = new ComboPooledDataSource();
 			try {
@@ -64,7 +87,7 @@ public class ConnectionFactory {
 			System.out.println("Você já está usando um dataSource");
 		}
 	}
-
+*/
 	public Connection getConnection() {
 		try {
 			System.out.println("Tentando iniciar conexao com o Banco INVICTO_DB");
