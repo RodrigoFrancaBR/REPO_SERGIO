e70211987ad41e31929bdc74b3f244d64c44f8b5
RodrigoFrancaBR <rfrancacosta@gmail.com>
2019-02-18 17:59:53 -0300

    Implementando o AuthService.

--- aplicacao_angular/invicto-vestibulares/src/app/core/auth.service.ts
+++ aplicacao_angular/invicto-vestibulares/src/app/core/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpCliente: HttpClient) { }
+
+  efetuarLogin(usuario: string, senha: string) {
+    this.httpCliente.post('')
+  }
 }
